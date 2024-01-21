e82ab5b762cf3f6dd7d71c27f4954f6c67d08537
ananyatewari <ananyatewari0205@gmail.com>
2024-01-22 01:59:25 +0530

    done

--- quiz/src/components/QuizComponent.jsx
+++ quiz/src/components/QuizComponent.jsx
@@ -1,22 +1,51 @@
 import React, { Component } from 'react'
+import Data from "../resources/Questions.json"
 
 export default class QuizComponent extends Component {
+  constructor(){
+    super()
+    this.state = {
+      count : 0
+    }
+  }
+
+  clickPrev = () => {
+    let newish = this.state.count == 0 ? Data.length - 1 : this.state.count - 1
+    this.setState({
+      count : newish
+    })
+  }
+
+  clickNext = () => {
+    let newish = this.state.count == Data.length - 1 ? 0 : this.state.count + 1
+    this.setState({
+      count : newish
+    })
+  }
+
+  clickQuit = () => {
+    window.confirm("Are you sure that you want to quit ?")
+  }
+
   render() {
     return (
       <div id='question'>
         <div><h2>Question</h2></div>
-        <div className='no'>1 of 15</div>
-        <div className='ques'>Which is the only mammal that can jump ?</div>
+        <div className='no'>
+          <span>{this.state.count + 1} </span>
+          of <span>{Data.length}</span>
+        </div>
+        <div className='ques'>{Data[this.state.count].question}</div>
         <div className="options">
-          <div>dog</div>
-          <div>elephant</div>
-          <div>goat</div>
-          <div>lion</div>
+          <div>{Data[this.state.count].optionA}</div>
+          <div>{Data[this.state.count].optionB}</div>
+          <div>{Data[this.state.count].optionC}</div>
+          <div>{Data[this.state.count].optionD}</div>
         </div>
         <div className="buttons">
-          <div>Previous</div>
-          <div>Next</div>
-          <div>Quit</div>
+          <div onClick={this.clickPrev}>Previous</div>
+          <div onClick={this.clickNext}>Next</div>
+          <div onClick={this.clickQuit}>Quit</div>
         </div>
       </div>
     )
